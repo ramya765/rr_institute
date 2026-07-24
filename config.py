6956e01ccc147df403050c16fdf76108371648b6
ramya765 <132564203+ramya765@users.noreply.github.com>
2026-07-24 11:43:27 +0530

    adding files without data

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     SECRET_KEY = "academy_website_2025_secret_key"
 
     SQLALCHEMY_DATABASE_URI = (
-        "mysql+pymysql://root:Lakshmi%4020033113@localhost/academy_db"
+        "mysql+pymysql://root:Parislove%40143@localhost/academy_db"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
