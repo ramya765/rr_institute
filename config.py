6c8b84e17cbeb50f389933f2ed5b4b1888060f5e
Divya <21pa1a05b1@vishnu.edu.in>
2026-07-24 10:44:00 +0530

    admin

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 class Config:
-    
+
     SECRET_KEY = "academy_website_2025_secret_key"
 
     SQLALCHEMY_DATABASE_URI = (
-    "mysql+pymysql://root:Parislove%40143@localhost/academy_db"
-)
+        "mysql+pymysql://root:Lakshmi%4020033113@localhost/academy_db"
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- routes/admin.py
+++ routes/admin.py
@@ -1,7 +1,57 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from models import Course, User
+from database import db
 
 admin = Blueprint("admin", __name__, url_prefix="/admin")
 
+
 @admin.route("/dashboard")
 def dashboard():
-    return render_template("admin/dashboard.html")
+
+    all_courses = Course.query.order_by(Course.created_at.desc()).all()
+
+    total_courses = Course.query.count()
+
+    total_students = User.query.filter_by(role="student").count()
+
+    return render_template(
+        "admin/dashboard.html",
+        courses=all_courses,
+        total_courses=total_courses,
+        total_students=total_students
+    )
+
+
+@admin.route("/courses")
+def courses():
+
+    all_courses = Course.query.all()
+
+    return render_template(
+        "admin/courses.html",
+        courses=all_courses
+    )
+
+
+@admin.route("/course/add", methods=["GET", "POST"])
+def add_course():
+
+    if request.method == "POST":
+
+        course = Course(
+            title=request.form["title"],
+            description=request.form["description"],
+            duration=request.form["duration"],
+            mode=request.form["mode"],
+            trainer=request.form["trainer"],
+            price=request.form["price"]
+        )
+
+        db.session.add(course)
+        db.session.commit()
+
+        flash("Course Added Successfully", "success")
+
+        return redirect(url_for("admin.dashboard"))
+
+    return render_template("admin/add_course.html")
